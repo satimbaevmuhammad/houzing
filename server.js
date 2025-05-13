dc3c91ba4d3ece9c0c41c87ce975fc955bc23924
satimbaevmuhammad <satimbaevmuhammad3@gmail.com>
2025-05-13 15:00:23 +0500

    done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
+
 const userRoutes = require('./routes/userRoutes');
 const houseRoutes = require('./routes/houseRoutes');
 
@@ -8,10 +10,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS middleware
+app.use(cors()); // 💡 Bu yerda CORS yoqiladi
+
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-app.use('/api/allHouse', houseRoutes); // <-- bu yerda allHouse route
+app.use('/api/allHouse', houseRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
